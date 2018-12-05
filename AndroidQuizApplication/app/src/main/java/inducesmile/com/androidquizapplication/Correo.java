91c7a2e53df0c91d6368f66e0d1ca89736449b33
MiltonDM <36650054+MiltonDM@users.noreply.github.com>
2018-12-04 20:29:53 -0600

    Add files via upload
    app del test de cisco

---
+++ AndroidQuizApplication/app/src/main/java/inducesmile/com/androidquizapplication/Correo.java
@@ -0,0 +1,13 @@
+package inducesmile.com.androidquizapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Correo extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email);
+    }
+    }
+
